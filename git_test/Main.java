4ee54a394063854a10baa2e7f0e04bbe5fa14d11
Dylan <kangjun5023@gmail.com>
2022-09-26 14:12:58 +0900

    Hello 클래스 수정

--- git_test/Main.java
+++ git_test/Main.java
@@ -2,8 +2,7 @@ package git_test;
 
 public class Main {
     public static void main(String[] args) {
-        Hello h = new Hello();
-        h.say();
-        }
-    
-}
+    Hello h = new Hello("hello world");
+    h.say();
+    }
+   }
